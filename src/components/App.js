c2a7d3449c3adb86977f5f5403e81b1fad29dc49
saivivek122 <119473212+saivivek122@users.noreply.github.com>
2025-09-26 11:47:42 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,43 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [todo, setTodo] = useState("");
+  const [addTodo, setAddTodo] = useState([]);
+  const [message,setMessage]=useState("")
+
+  function handleTodo(event) {
+    // console.log(event.target.value);
+    setTodo(event.target.value);
+  }
+  function handleSubmit() {
+    if(!todo) {
+      setMessage("Enter Todo");
+      return
+    }
+    setAddTodo([...addTodo, todo]);
+    // console.log("from", addTodo);
+    setTodo("");
+    setMessage("")
+  }
+  function deleteTodo(item) {
+    console.log(item);
+    let filteredTodo = addTodo.filter((data, index) => index != item);
+    setAddTodo(filteredTodo);
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+    <div className="container">
+      <h1>To-Do-List</h1>
+      <div className="todo-container">
+        <input onChange={handleTodo} value={todo} className="input" />
+        <button onClick={handleSubmit} className="button">
+          Add Todo
+        </button>
+      </div>
+      <p>{message}</p>
+      <Todo todoItems={addTodo} deleteTodo={deleteTodo} />
+    </div>
     </div>
   )
 }
